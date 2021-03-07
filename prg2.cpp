f5732f7b0ad06a6923497b3f50cc505c0966e757
Atharav Upare <atharavpare@gmail.com>
2021-03-07 23:55:49 +0530

    Even odd program

---
+++ prg2.cpp
@@ -0,0 +1,19 @@
+//Program to Check Whether Number is Even or Odd.
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num;
+  cout<<"Enter the number";
+  cin>>num;
+  if(num%2==0)
+  {
+    cout<<"The Number is Even";
+  }
+  else
+  {
+    cout<<"The Number is odd";
+  }
+  return 0;
+  
+}
