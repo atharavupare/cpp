c9943ce1f57358901f730ec5ebdb201519eeefaf
Atharav Upare <atharavpare@gmail.com>
2021-03-08 22:44:44 +0530

    Natural Number

---
+++ prg3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n,sum;
+cout<<"Enter the to check:";
+cin>>n;
+if(n == 0)
+{
+  cout<<"The Number is not Natural Number .";
+
+}
+else
+{
+  cout<<"The Number is Natural Number";
+
+}
+
+return 0;
+
+}
